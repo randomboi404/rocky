37005e54240add9a1b7134b19aa174af5b7f06a6
Harsh Mehta <147304413+HarzhMehta@users.noreply.github.com>
2026-06-02 19:21:03 +0530

    add argparse functionality

--- src/main.c
+++ src/main.c
@@ -9,6 +9,34 @@
 #include <rocky/main.h>
 #include <rocky/parser/parser.h>
 
+/*#include <llvm-c/Core.h>
+#include <llvm-c/Types.h>
+#include <llvm-c/Target.h>
+#include <llvm-c/TargetMachine.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <rocky/main.h>
+#include <rocky/jit.h>
+
+typedef void printnum_fn(int p);
+
+int main() {
+    JITContext jit = {0};
+    jit_init(&jit);
+
+    jit_add_dummy_functions(&jit);
+    jit_bake(&jit);
+
+    printnum_fn* fn = (printnum_fn*) jit_lookup_function(&jit, "printnum");
+    fn(10);
+    fn(20);
+    fn(30);
+
+    jit_free(&jit);
+
+    hello_world();*/
+
 static char *read_entire_file(const char *path) {
     FILE *fp = fopen(path, "rb");
     if (!fp) return NULL;
